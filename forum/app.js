41afa88a6171154bcb4bd167bdb4927ce8a9bd06
Jonathan Hsu <jonshsu@gmail.com>
2019-05-14 02:46:12 -0700

    Update app.js

--- forum/app.js
+++ forum/app.js
@@ -67,5 +67,16 @@ app.post('/newpost', newpostHandler);
 // app.get('/translate', queryHandler );   // if not, is it a valid query?
 app.use( fileNotFound );            // otherwise not found
 
-app.listen(port, function (){console.log('Listening...');} )
+// app.listen(port, function (){console.log('Listening...');} )
  
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  const server = app.listen(process.env.PORT || 8080, () => {
+    const port = server.address().port;
+    console.log(`App listening on port ${port}`);
+  });
+  // [END server]
+}
+
+module.exports = app;
