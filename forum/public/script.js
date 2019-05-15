f65ef31697e393ce1d5f5b1c1a476aca394729c4
Jonathan Hsu <jonshsu@gmail.com>
2019-05-14 19:26:27 -0700

    removed reloading

--- forum/public/script.js
+++ forum/public/script.js
@@ -19,16 +19,48 @@ function createCORSRequest(method, url) {
 	return xhr;
 }
 
-function sendnewpost() {
+function removepostsfrompage() {
+	toremove = document.getElementsByClassName("post");
+	while (toremove.length > 0) {
+		toremove[0].remove();
+	}
+}
 
+function clearform() {
+	document.getElementById('new_post_button').value = 'Create Post'; 
+	document.getElementById('new_post_title').value = ''; 
+	document.getElementById('new_post_content').value = ''; 
+
+}
+
+function sendnewpost() {
+	
 	// get text
-	let title = document.getElementById("new_post_title").value;
-	let content = document.getElementById("new_post_content").value;
+	let title = document.getElementById("new_post_title");
+	let content = document.getElementById("new_post_content");
 	let mydata = {
-		"title": title,
-		"content": content
+		"title": title.value,
+		"content": content.value
 	};
 
+	// // input validation
+	// title.style.borderBottom = "";
+	// content.style.borderBottom = "";
+
+	// if(title.value == '') {
+	// 	title.style.borderBottom = "1px solid red";
+	// }
+	// if(content.value == '') {
+	// 	content.style.borderBottom = "1px solid red";
+	// }
+	// if(title.value == '' || content.value == '') {
+	// 	return false;
+	// }
+
+
+	// visual things
+	document.getElementById('new_post_button').value = 'Creating...'; 
+	// document.getElementById('createanimation').style.margin = 0; 
 
 	let url = "/newpost";
 	let xhr = createCORSRequest('POST', url);
@@ -42,7 +74,13 @@ function sendnewpost() {
 		let responseStr = xhr.responseText;  // get the JSON string 
 		let status = xhr.status; 
 		console.log(responseStr, status);
-		document.location.reload(false);
+
+
+		// document.location.reload(false);
+		removepostsfrompage();
+		getposts();
+
+		clearform();
 	};
 
 	xhr.onerror = function() {
@@ -94,11 +132,12 @@ function insertposts(object) {
 
 		let div_bar = document.createElement('div');
 		div_bar.className = "bar";
-		div_bar.textContent = "Author";
+		div_bar.textContent = "Anonymous";
 
+		let formatteddate = new Date(object[i].date.slice(1, -1));
 		let span_datetime = document.createElement('span');
 		span_datetime.className = "datetime";
-		span_datetime.textContent = object[i].date.slice(1, -1);
+		span_datetime.textContent = formatteddate.toLocaleString();
 
 		let h3_title = document.createElement('h3');
 		// h3_title.className = "title";
@@ -169,9 +208,9 @@ function clearposts() {
 		// let object = JSON.parse(responseStr);  // turn it into an object
 		console.log(responseStr);
 		
-		document.location.reload(false);
-		// let output = document.getElementById("outputGoesHere");
-		// output.textContent = object.palindrome;
+		// document.location.reload(false);
+		removepostsfrompage();
+		getposts();
 	};
 
 	xhr.onerror = function() {
